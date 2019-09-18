8455669e7d82e4395a7344ecc89f916e5faf9b07
Ziyu Chen <ziychen@umich.edu>
2019-09-18 15:37:43 -0400

    Check if Question

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,13 @@
+import random
 def ask_question():
     question_inp = input("Please enter your question: ")
     return question_inp
+
+while True:
+    ques = ask_question()
+    if ques[-1] == "?":
+        print("Yes it's a question.")
+    elif ques == "quit":
+        break
+    else:
+        print("I'm sorry, I can only answer questions.")
